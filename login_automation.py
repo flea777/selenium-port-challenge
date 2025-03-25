f16ccc9c0e8e80a5e57e3ed50cf4ae5958f9d375
Pedro <pedrodinizz2@outlook.com>
2025-03-25 19:11:46 -0300

    login elements located and submited

--- login_automation.py
+++ login_automation.py
@@ -15,4 +15,9 @@ url = 'https://www.saucedemo.com/'
 driver.get(url)
 driver.maximize_window()
 
+username_input = wait.until(EC.visibility_of_element_located((By.ID, 'user-name')))
+password_input = wait.until(EC.visibility_of_element_located((By.ID, 'password')))
 
+username_input.send_keys(os.environ['USERNAME'])
+password_input.send_keys(os.environ['PASSWORD'])
+password_input.submit()
